e64c6894920eb16553b3c943d1dfac9e4898b73a
Aziel Ferguson <azielferguson@gmail.com>
2019-12-25 23:57:30 -0500

    Added example of canvas draw

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import mapImage from '../assets/images/netherlands_map.jpg'
+
+const pinRect = {
+    style: 'green',
+    width: 5,
+    height: 5
+}
+
+const pins = [
+    {xcoord: 25, ycoord: 30},
+    {xcoord: 50, ycoord: 30},
+    {xcoord: 75, ycoord: 30},
+    {xcoord: 50, ycoord: 60},
+]
 
 export class InteractiveMap extends React.Component {
     constructor(props){
@@ -7,11 +21,19 @@ export class InteractiveMap extends React.Component {
         this.imageRef = React.createRef();
     }
     
+    drawImageContextAndPins(context, image, pins) {
+        context.drawImage(image, 0, 0)
+        context.fillStyle = pinRect.style;
+        pins.map( pin => {
+            return context.fillRect(pin.xcoord, pin.ycoord, pinRect.width, pinRect.height);
+        })
+    }
+
     componentDidMount() {
         const canvas = this.mapRef.current;
         const img = this.imageRef.current;
         const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
+        img.onload = () => (this.drawImageContextAndPins(ctx, img, pins));
     }
       
     render() {
@@ -19,7 +41,12 @@ export class InteractiveMap extends React.Component {
         return (
             <div>
                 <canvas ref={this.mapRef} width={640} height={425} />
-                <img ref={this.imageRef} src={'../../public/netherlands_map.jpg'} className='hidden' />
+                <img
+                    ref={this.imageRef} 
+                    src={mapImage}
+                    alt=''
+                    hidden={true}
+                />
             </div>
         );
     };
